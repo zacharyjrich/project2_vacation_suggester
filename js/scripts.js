a7ef54c6d01bd14c3cd37b62d88fda2bc4f3f619
Zachary Richards <zachary.j.richards@gmail.com>
2016-06-16 07:09:09 -0700

    add jquery to show result of survey when submitted

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
   $("#ready_button").click(function() {
-    $(".survey").show();
+    $("#survey").show();
     $(".intro").hide();
+  });
+
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var company = $("input:radio[name=company]:checked").val();
+    var music = $("input:radio[name=music]:checked").val();
+    var pizza = $("input:radio[name=pizza]:checked").val();
+    var movie = $("input:radio[name=movie]:checked").val();
+    var silly = $("input:radio[name=silly]:checked").val();
+
+    if (company === "alone" && music === "beatles" && pizza === "utensils" && movie === "shawshank" && silly === "numbers") {
+      $("#answer1").show();
+      $("#survey").hide();
+    } else {
+      $("#answer2").show();
+      $("#survey").hide();
+    }
+
   });
 
 });
